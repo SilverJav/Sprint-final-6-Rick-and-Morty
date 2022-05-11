96c1bedeb14f322f594d803d6ebbdf60d5488ddd
Cesar <camongez28@gmail.com>
2022-05-11 13:16:19 -0400

    Componentes separados2

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -4,15 +4,13 @@
         <input class="form-control me-2" v-model="filtro" type="text" placeholder="Buscar..." aria-label="Search">
         <button class="btn btn-outline-success" type="submit">Search</button>
   </div>
-    <h1 class="text-center mb-5">CHARACTERS RICK AND MORTY</h1>
+    <h1 class="text-center mb-5">RICK AND MORTY</h1>
     <div class="row justify-content-around">
       <div class="col-9 col-sm-4 col-md-3 col-lg-2 px-4 pb-2" v-for="(item, index) in filtroNombres" :key="index">
         <div class="card ">
           <img class="card-img-top" v-bind:src="item.image" v-bind:alt="item.name"/>
         <div class="card-body ">
           <h5 class="card-title text-center ">{{ item.name }}</h5>
-         
-          
         </div>
         <div class="d-flex justify-content-center flex-wrap">
           <div class="d-flex justify-content-around w-100 ">
