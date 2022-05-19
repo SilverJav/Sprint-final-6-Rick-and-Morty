981876ca0dc63512cba091521de7d637a3bc3ff3
Javiera <ariel.abarca7@gmail.com>
2022-05-19 19:26:56 -0400

    Arreglo de detalles (ortografía)

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,9 +1,10 @@
 <template>
-
+<div class="container">
   <div class="about">
     <h1>Acerca de Nosotros</h1>
+    <p>Sprint final módulo 6 Terminado</p>
 
-    
+  </div>
   </div>
 </template>
 <script>
